d9e82bb43bfed08ee373605b4a62b96112f20507
Dan K <KC135Q@users.noreply.github.com>
2021-05-18 09:30:29 -0400

    PORT Must be 8080 due to access settings

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { FluentTree } from './FluentTree';
 import { FluentFile } from './FluentFile';
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 // Set interval time range (min and max)
 const fiveMinutes = 300000;
