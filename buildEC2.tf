96e4e838df25dc60689533c9133759d88851ff81
Dan K <KC135Q@users.noreply.github.com>
2021-05-18 14:11:24 -0400

    Updated outbound traffice settings

--- buildEC2.tf
+++ buildEC2.tf
@@ -33,6 +33,14 @@ resource "aws_security_group" "testInstanceSG" {
    protocol    = "tcp"
    cidr_blocks = ["0.0.0.0/0"]
   }
+  # All Traffic default outbound rule
+    egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
 }
 
 resource "aws_eip" "lb" {
